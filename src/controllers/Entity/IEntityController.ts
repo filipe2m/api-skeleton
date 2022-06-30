b095a3140a2edadef87e8bf15446367d7c131536
Filipe Mendes <filipemendes@upskill.pt>
2022-06-30 20:19:48 +0100

    FIX: Removal of an outdated function

--- src/controllers/Entity/IEntityController.ts
+++ src/controllers/Entity/IEntityController.ts
@@ -1,4 +1,3 @@
-import EntityController from "./EntityController";
 import { Request, Response, NextFunction } from 'express';
 
 export default interface IEntityController {
@@ -8,8 +7,4 @@ export default interface IEntityController {
     put: (req: Request, res: Response, next: NextFunction) => Promise<any>;
     patch: (req: Request, res: Response, next: NextFunction) => Promise<any>;
     delete: (req: Request, res: Response, next: NextFunction) => Promise<any>;
-}
-
-export const entityController = () => {
-    return new EntityController();
 }
